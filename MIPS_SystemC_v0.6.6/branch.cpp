d38af872b48aec3a36f40c035c671f466579b9d7
Bruno Silva <brunomiguelsilva@ua.pt>
2015-10-28 23:08:40 +0000

    fixed jump proble

--- MIPS_SystemC_v0.6.6/branch.cpp
+++ MIPS_SystemC_v0.6.6/branch.cpp
@@ -7,17 +7,21 @@
 void branch::branch_detect()
 {
     sc_uint<32> target32 = jtarget.read();
+
     switch(opcode.read())
     {
         case 2:
+            perror("J");
+            fprintf(stderr, "jtarget: %#08x\n", (int)(jtarget.read()));
+            fprintf(stderr, "pc4 %#08x\n", (int)(PC4.read()));
+            fprintf(stderr, "target32 %#08x\n", (int)(target32));
             BranchTaken.write(true);
-            BranchTarget.write(target32 | (PC4.read() & 0xF000));
+            BranchTarget.write(target32 | (PC4.read() & 0xFC000000));
+
             break;
 
         case 4:
-            perror("BEQ");
             if (rs.read() == rt.read() && branch_in.read() == true){
-                perror("ENTROU");
                 BranchTaken.write(true);
                 BranchTarget.write(PC4.read() + (imm_ext.read() << 2));
             }
